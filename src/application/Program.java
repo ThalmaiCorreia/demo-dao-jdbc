217242ec62304a35d6b554fdb41f7e69729a677c
Thalmai Correia <thalmaicorreia7@gmail.com>
2023-06-30 23:21:55 -0300

    Implementou delete Seller.

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,39 +7,50 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args){
 
+        Scanner sc = new Scanner(System.in);
+
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        System.out.println("==== Teste 1: Seller findById ====");
+        System.out.println("==== Teste 1: Seller FindById ====");
         Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
 
-        System.out.println("\n==== Teste 2: Seller findByDepartment ====");
+        System.out.println("\n==== Teste 2: Seller FindByDepartment ====");
         Department department = new Department(2, null);
         List<Seller> list = sellerDao.findByDepartment(department);
         for (Seller obj : list){
             System.out.println(obj);
         }
-        System.out.println("\n==== Teste 3: Seller findAll ====");
+        System.out.println("\n==== Teste 3: Seller FindAll ====");
         list = sellerDao.findAll();
         for (Seller obj : list){
             System.out.println(obj);
         }
-        System.out.println("\n==== Teste 4: Seller insert ====");
+        System.out.println("\n==== Teste 4: Seller Insert ====");
         Seller newSeller = new Seller(null, "Tom", "tom@gmail.com", new Date(), 3500.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserido! Novo id = " + newSeller.getId());
 
-        System.out.println("\n==== Teste 5: Seller update ====");
+        System.out.println("\n==== Teste 5: Seller Update ====");
         seller = sellerDao.findById(1);
         seller.setName("Antonio Mario");
         sellerDao.update(seller);
         System.out.println(seller);
         System.out.println("Atualizado com Sucesso.");
+
+        System.out.println("\n==== Teste 6: Seller Delete ====");
+        System.out.println("Entre o Id para ser deletado: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Deletado com sucesso.");
+
+        sc.close();
     }
 }
--- src/model/impl/SellerDaoJDBC.java
+++ src/model/impl/SellerDaoJDBC.java
@@ -86,7 +86,24 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void deleteById(Integer id) {
+         PreparedStatement st = null;
+         try{
+             st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
 
+             st.setInt(1, id);
+
+             int rowsAffected = st.executeUpdate();
+
+             if (rowsAffected < 1){
+                 throw new DbException("Id indicado não existe.");
+             }
+         }
+         catch (SQLException e){
+             throw new DbException(e.getMessage());
+         }
+         finally {
+             DB.closeStatement(st);
+         }
     }
 
     @Override
