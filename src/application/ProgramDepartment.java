03367b53f028f8f0769451e8344e0e7d1e5299c2
Thalmai Correia <thalmaicorreia7@gmail.com>
2023-07-19 20:49:07 -0300

    Implementou Update e DeleteById Department.

--- src/application/ProgramDepartment.java
+++ src/application/ProgramDepartment.java
@@ -6,11 +6,13 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class ProgramDepartment {
     public static void main(String[] args) {
 
 
+        Scanner sc = new Scanner(System.in);
 
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
@@ -25,9 +27,27 @@ public class ProgramDepartment {
         }
 
         System.out.println("\n==== Teste 4: Department Insert ====");
-        Department newDepartment = new Department(null, "Game");
+        Department newDepartment = new Department(null, "Fashion");
         departmentDao.insert(newDepartment);
         System.out.println("Inserido! Departamento = " + newDepartment.getName() + " Id = " + newDepartment.getId());
 
+        System.out.println("\n==== Teste 5: Department Update ====");
+        department = departmentDao.findById(3);
+        department.setName("Films");
+        departmentDao.update(department);
+        System.out.println(department);
+        System.out.println("Atualizado com Sucesso.");
+
+        System.out.println("\n==== Teste 6: Department Delete ====");
+        System.out.println("Entre com o Id a ser deletado: ");
+        int id = sc.nextInt();
+        departmentDao.deleteById(id);
+        System.out.println("Deletado com sucesso!");
+        list = departmentDao.findAll();
+        for (Department dep : list){
+            System.out.println(dep);
+        }
+
+        sc.close();
     }
     }
